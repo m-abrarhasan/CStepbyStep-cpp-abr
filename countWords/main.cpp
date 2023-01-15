069eb0569fdadf58de3224942453dcdd378c5116
Abrar <61474167+m-abrarhasan@users.noreply.github.com>
2023-01-15 11:35:13 +0600

    Update main.cpp

--- countWords/main.cpp
+++ countWords/main.cpp
@@ -1,37 +1,34 @@
-#include <iostream>
 #include <cctype>
+#include <iostream>
 
-size_t countWords(std::string str)
-{
-    size_t words = 0;
+size_t countWords(std::string str) {
+  size_t words = 0;
 
-    // In case of an empty string apending two spaces
-    // to make sure index 1 exists
-    str.append("  ");
+  // In case of an empty string apending two spaces
+  // to make sure index 1 exists
+  str.append("  ");
 
-    for (int i = 1; i < str.length(); ++i)
-    {
-        if (' ' == str.at(i) && ' ' != str.at(i - 1))
-        {
-            words++;
-        }
+  const size_t lenstr = str.length();
+  for (int i = 1; i < lenstr; ++i) {
+    if (' ' == str.at(i) && ' ' != str.at(i - 1)) {
+      words++;
     }
+  }
 
-    return words;
+  return words;
 }
 
 // Main
 ///////////////////////////////////////////////////////
-int main()
-{
-    std::string msg;
+int main() {
+  std::string msg;
 
-    std::cout << "Your String : ";
-    std::getline(std::cin, msg);
+  std::cout << "Your String : ";
+  std::getline(std::cin, msg);
 
-    size_t words = countWords(msg);
+  size_t words = countWords(msg);
 
-    std::cout << words << std::endl;
+  std::cout << words << std::endl;
 
-    return 0;
+  return 0;
 }
