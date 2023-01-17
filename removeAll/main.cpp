4d79501c3bb9977189e83a65ee23cadccbf3227b
Abrar <61474167+m-abrarhasan@users.noreply.github.com>
2023-01-17 18:52:36 +0600

    solve removeAll

---
+++ removeAll/main.cpp
@@ -0,0 +1,33 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+std::string removeAll(std::string str, const char &t) {
+
+  auto strlen = str.length();
+
+  size_t i = 0;
+  while (i < strlen) {
+
+    std::cout << str[i] << " | " << t << "   " << strlen << "\n";
+    if (str.at(i) == t) {
+
+      str.erase(i, 1);
+      strlen--;
+
+    } else
+      i++;
+  }
+
+  return str;
+}
+
+// Main
+///////////////////////////////////////////////////////
+int main() {
+
+  std::string msg = removeAll("---0---", '-');
+
+  std::cout << msg;
+  return 0;
+}
