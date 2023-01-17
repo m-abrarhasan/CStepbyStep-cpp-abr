4fa0106dbb63411e0cacd60271c94325f98ce365
Abrar <61474167+m-abrarhasan@users.noreply.github.com>
2023-01-17 18:52:24 +0600

    solve printBackward

---
+++ printBackward/main.cpp
@@ -0,0 +1,16 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+void printBackward(std::string str) {
+
+  std::reverse(str.begin(), str.end());
+  std::cout << str;
+}
+
+// Main
+///////////////////////////////////////////////////////
+int main() {
+  printBackward("hello there!");
+  return 0;
+}
