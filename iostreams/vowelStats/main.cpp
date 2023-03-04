7989c97396cc626d8a56566ec7dfff243c7f990a
Abrar <61474167+m-abrarhasan@users.noreply.github.com>
2023-03-04 17:24:13 +0600

    use unordered_set

--- iostreams/vowelStats/main.cpp
+++ iostreams/vowelStats/main.cpp
@@ -1,49 +1,49 @@
-#include <algorithm>
-#include <array>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 void vowelStats(const std::string& path)
 {
     std::ifstream file(path);
     if (file.is_open()) {
 
-        const std::array<char, 5> vowel { 'A', 'E', 'I', 'O', 'U' };
-        size_t total_l   = 0;
-        size_t longest_l = 0;
-        size_t total_ch  = 0;
-        double average   = 0;
+        const std::unordered_set<char> vowel { 'A', 'E', 'I', 'O', 'U' };
+        size_t total_ln = 0;
+        size_t longest  = 0;
+        size_t total_ch = 0;
+        double average  = 0;
 
         std::string line;
         while (std::getline(file, line)) {
 
-            total_l++;
+            total_ln++;
             total_ch += line.length();
-            longest_l = (line.length() > longest_l) ? line.length() : longest_l;
+            longest = (line.length() > longest) ? line.length() : longest;
 
             size_t vowels = 0;
             for (const auto& c : line) {
-                char u = std::toupper(c);
-                vowels += std::count(vowel.begin(), vowel.end(), u);
+                vowels += vowel.count(std::toupper(c));
             }
 
-            std::cout << "Line " << total_l
+            std::cout << "Line " << total_ln
                       << " has " << line.length()
                       << " chars and " << vowels
                       << " vowels\n";
         }
-        average = total_ch / static_cast<double>(total_l);
-        std::cout << total_l
+        average = total_ch / static_cast<double>(total_ln);
+        std::cout << total_ln
                   << " lines; longest = "
-                  << longest_l
+                  << longest
                   << ", average = "
                   << average;
     }
 }
 
+/////////////////////
 int main(int, char**)
 {
     vowelStats("../input.txt");
     return 0;
 }
+/////////////////////
